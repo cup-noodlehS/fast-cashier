8769d1050d6bad7e2e318ce29aef43c1e9369b07
Sheldon Sagrado <sheldon.bposeats@gmail.com>
2024-09-01 21:40:05 +0800

    add navbar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-09-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss'],
-  css: ['@/public/global.scss'],
-})
+    compatibilityDate: "2024-09-01",
+    devtools: { enabled: true },
+    modules: ["@nuxt/ui", "@nuxtjs/tailwindcss"],
+    css: ["@/public/global.scss"],
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,56 @@
 <template>
-    <div class="bg-white h-lvh max-h-lvh">
-        <h1 class="text-3xl">hi</h1>
+    <div class="bg-white h-lvh max-h-lvh overflow-hidden">
+        <nav
+            class="w-screen max-w-screen flex justify-between items-center px-4 py-3 bg-violet-100 border border-b-violet-300"
+        >
+            <div class="flex items-center gap-5">
+                <UIcon name="i-heroicons-bars-3" class="w-6 h-6" />
+                <div class="flex items-center gap-2">
+                    <h1 class="text-2xl font-bold">🍲 Logo</h1>
+                    <UIcon
+                        name="i-heroicons-lock-open"
+                        class="h-6 w-6 text-violet-800 cursor-pointer"
+                    />
+                </div>
+            </div>
+            <div class="flex items-center gap-3">
+                <UButton
+                    icon="i-heroicons-chart-bar-square"
+                    size="xl"
+                    color="black"
+                    variant="outline"
+                    label="Analytics"
+                    :trailing="false"
+                />
+                <UButton
+                    icon="i-heroicons-receipt-percent"
+                    size="xl"
+                    color="black"
+                    variant="outline"
+                    label="Orders"
+                    :trailing="false"
+                />
+                <UButton
+                    icon="i-heroicons-plus"
+                    size="xl"
+                    color="primary"
+                    variant="solid"
+                    label="Add item"
+                    :trailing="false"
+                />
+            </div>
+        </nav>
+        <main class="flex h-full">
+            <section class="w-[300px] h-full border border-r-violet-300">
+                <img :src="sampleImage" alt="sample" />
+            </section>
+            <section></section>
+        </main>
     </div>
 </template>
 
 <script setup lang="ts">
-import type { ProductInterface } from "@/interfaces/pos.ts";
+import sampleImage from "@/public/images/chicken.jpg";
 </script>
 
 <style scoped></style>
